029bfe0ebbf44b55008b1c11b9d72b8ad21db9c0
raymond <raymond.korir@student.moringaschool.com>
2024-04-24 08:52:56 +0300

    sending-data-with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+      .catch(function (error) {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+  
